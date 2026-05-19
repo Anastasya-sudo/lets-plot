68566851a145cdb5a48a80f06f691b80a0ef1e2a
Anastasya-sudo <stasya-orient@yandex.ru>
2026-05-19 13:25:12 +0300

    Add primitive factory methods (rectangle, arc, circle) to GeomStyle

--- plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/render/svg/GeomStyle.kt
+++ plot-base/src/commonMain/kotlin/org/jetbrains/letsPlot/core/plot/base/render/svg/GeomStyle.kt
@@ -5,10 +5,30 @@
 
 package org.jetbrains.letsPlot.core.plot.base.render.svg
 
+import org.jetbrains.letsPlot.commons.geometry.DoubleRectangle
 import org.jetbrains.letsPlot.commons.geometry.DoubleVector
+import org.jetbrains.letsPlot.core.plot.base.geom.util.approximateArc
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 interface GeomStyle {
     fun resamplePath(points: List<DoubleVector>): List<DoubleVector>
+
+    fun rectangle(rect: DoubleRectangle): List<DoubleVector> =
+        resamplePath(rectanglePoints(rect))
+
+    fun arc(
+        startPoint: DoubleVector,
+        endPoint: DoubleVector,
+        startAngle: Double,
+        endAngle: Double,
+        arcPoint: (Double) -> DoubleVector
+    ): List<DoubleVector> =
+        resamplePath(approximateArc(startPoint, endPoint, startAngle, endAngle, arcPoint))
+
+    fun circle(center: DoubleVector, radius: Double): List<DoubleVector> =
+        resamplePath(approximateCircle(center, radius))
 
     object Regular : GeomStyle {
         override fun resamplePath(points: List<DoubleVector>): List<DoubleVector> = points
@@ -18,4 +38,23 @@ interface GeomStyle {
         override fun resamplePath(points: List<DoubleVector>): List<DoubleVector> =
             XkcdPathEffect.toHandDrawn(points)
     }
+}
+
+private fun rectanglePoints(rect: DoubleRectangle): List<DoubleVector> = listOf(
+    DoubleVector(rect.left, rect.top),
+    DoubleVector(rect.right, rect.top),
+    DoubleVector(rect.right, rect.bottom),
+    DoubleVector(rect.left, rect.bottom),
+    DoubleVector(rect.left, rect.top)
+)
+
+private fun approximateCircle(center: DoubleVector, radius: Double): List<DoubleVector> {
+    val arcPoint = { angle: Double ->
+        center.add(DoubleVector(radius * cos(angle), radius * sin(angle)))
+    }
+    val right = arcPoint(0.0)
+    val left = arcPoint(PI)
+    val upperArc = approximateArc(right, left, 0.0, PI, arcPoint)
+    val lowerArc = approximateArc(left, right, PI, 2.0 * PI, arcPoint)
+    return upperArc + lowerArc.drop(1)
 }
